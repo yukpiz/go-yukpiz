a21e0a7010ad5a409e91eea0091456f3b8781892
yukpiz <yukpiz@gmail.com>
2018-08-03 20:10:26 +0900

    Updated.

--- yukpiz.go
+++ yukpiz.go
@@ -1,5 +1,9 @@
 package yukpiz
 
+import (
+	"fmt"
+)
+
 func UniqInts(values []int) []int {
 	m := make(map[int]bool)
 	uniq := []int{}
@@ -10,4 +14,18 @@ func UniqInts(values []int) []int {
 		}
 	}
 	return uniq
+}
+
+func FizzBuzz(n int) {
+	for i := 1; i <= n; i += 1 {
+		if i%15 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
 }
